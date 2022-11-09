0aaf01cb54b4311ef425b38d5e503ceed28183cc
NishaBisht493 <81354503+NishaBisht493@users.noreply.github.com>
2022-11-09 16:25:29 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,12 @@
+function over() {
+  document.getElementById("image").src = "/Images/clinic2.png";
+}
+function out() {
+  document.getElementById("image").src = "/Images/clinic.png";
+}
+
+// function click() {
+//   document.getElementById("image").src = "/Images/clinic3.png";
+// }
+// document.getElementById("image").addEventListener("click", click);
+
